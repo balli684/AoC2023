af679a69eb06041c4afdb4a623ffd5869610fb65
Niels Ballis <niels@ballis.nl>
2023-12-05 22:11:27 +0100

    day 4 problem 1

---
+++ Day4_1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = "../../../Input.txt";
+
+        string[] input = File.ReadAllLines(filePath);
+        
+        int count = 0;
+
+        foreach (string line in input)
+        {
+            var splitCard = line.Trim().Split(':');
+            var splitNumbers = splitCard[1].Trim().Split('|');
+            string[] winningStr = splitNumbers[0].Trim().Split(' ');
+            string[] resultsStr = splitNumbers[1].Trim().Split(' ');
+
+            winningStr = winningStr.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+            resultsStr = resultsStr.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+
+            int card = 0;
+
+            foreach (string result in resultsStr)
+            {
+                if (winningStr.Contains(result))
+                {
+                    if (card == 0)
+                    {
+                        card++;
+                    }
+                    else
+                    {
+                        card *= 2;
+                    }
+                }
+            }
+            
+            count += card;
+            
+        }
+        Console.WriteLine(count);
+    }    
+
+}
