94f7267f2998028adad6f271ca8ca52e5876bc8a
Niels Ballis <niels@ballis.nl>
2023-12-06 23:44:34 +0100

    day 5 problem 1

--- Day5_1/Program.cs
+++ Day5_1/Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Xml;
-
+﻿
 class Program
 {
     static void Main()
@@ -9,22 +6,61 @@ class Program
         string filePath = "../../../Input.txt";
         string[] input = File.ReadAllLines(filePath);
 
-        // Parse seeds
-        // string[] seedsStrings = input[0].Split(':')[1].Trim().Split(' ');
-
-        // Int64[] seeds = Array.ConvertAll(seedsStrings, Int64.Parse);
-
         long[] seeds = Array.ConvertAll(input[0].Split(':')[1].Trim().Split(' '), long.Parse);
 
-        for (int i = 3; i < input.Length; i++)
+        List<List<long[]>> maps = [];
+
+        long min = 9223372036854775807;
+
+        int i = 3;
+        do
         {
-            if(input[i] != "")
+            
+            List<long[]> map = [];
+
+            while(i < input.Length && input[i] != "")
             {
-                Console.WriteLine(input[i]);
+                long[] row = Array.ConvertAll(input[i].Split(' '), long.Parse);
+                map.Add(row);
+                i++;
+            }
+            maps.Add(map);
+            i += 2;
+        } while (i < input.Length);
+
+        
+        foreach (var seed in seeds)
+        {
+            var location = seed;
+            int mapCounter = 0;
+            while (mapCounter < maps.Count)
+            {
+                int rowCounter = 0;
+                
+                while (rowCounter < maps[mapCounter].Count)
+                {
+                    if (location >= maps[mapCounter][rowCounter][1] && location <= maps[mapCounter][rowCounter][1] + maps[mapCounter][rowCounter][2])
+                    {
+                        location = location + maps[mapCounter][rowCounter][0] - maps[mapCounter][rowCounter][1];
+                        rowCounter = maps[mapCounter].Count;
+
+                    }
+                    else
+                    {
+                        rowCounter++;
+                    }
+                }
+                mapCounter++;
+                
+            }
+            if (location < min)
+            {
+                min = location;
             }
         }
-            
-        
+
+        Console.WriteLine(min);
+
     }
 }
 
