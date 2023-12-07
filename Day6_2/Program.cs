ea5f4be34ba2ea06d34d33148e2d7aeedec937df
Niels Ballis <n.ballis@das.nl>
2023-12-07 13:36:10 +0100

    day 6 problem 2

---
+++ Day6_2/Program.cs
@@ -0,0 +1,39 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        // string filePath = "../../../Input.txt";
+        // string[] input = File.ReadAllLines(filePath);
+
+        // int[ , ] input = { { 7, 9}, { 15, 40 }, { 30, 200 } };
+
+        //Time:        56     97     78     75
+        //Distance:   546   1927   1131   1139
+
+        // long time = 71530;
+        // long record = 940200;
+
+        long time = 56977875;
+        long record = 546192711311139;
+
+
+
+        long win = 0;
+        for (long hold = 1; hold < time; hold++)
+        {
+            if ((time - hold) * hold > record){
+                win++;
+            }
+            else
+            {
+                if (win > 0)
+                {
+                    hold = time;
+                }
+            }
+        }
+
+        Console.WriteLine(win);
+    }
+}
