4bdd8035ae617a6afcda1889c6885d910370526e
Niels Ballis <n.ballis@das.nl>
2023-12-11 10:10:29 +0100

    day 9 problem 2

---
+++ Day9_2/Program.cs
@@ -0,0 +1,71 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+
+        string filePath = "../../../Input.txt";
+        List<string> input = [.. (File.ReadAllLines(filePath))];
+
+        int output = 0;
+
+        foreach (var line in input)
+        {
+            // Console.WriteLine(line);
+            List<List<int>> x = [];
+            List<int> ints = line
+                        .Split(" ")
+                        .Select(s => Int32.TryParse(s, out int n) ? n : (int?)null)
+                        .Where(n => n.HasValue)
+                        .Select(n => n.Value)
+                        .ToList();
+            
+            x.Add(ints);
+
+            // while(Enumerable.Sum(x[^1]) != 0)
+            while(x[^1].Any(o => o != 0))
+            {
+                List<int> y = [];
+                int i = 0;
+
+                while (i < x[^1].Count - 1)
+                {
+                    // y.Add(Math.Abs(x[^1][i] - x[^1][i+1]));
+                    y.Add(x[^1][i+1] - x[^1][i]);
+                    i++;
+                }
+                x.Add(y);
+            }
+
+            x.RemoveAll(s => s == null);
+
+            // foreach(var a in x)
+            // {
+            //     foreach(var b in a)
+            //     {
+            //         Console.Write("{0} ",b);
+            //     }
+            //     Console.WriteLine();
+                
+            // }
+            
+
+            int j = 0;
+            // foreach(var z in x)
+            while (j < x.Count)
+            {
+                if (j%2 == 0){
+                    output += x[j][0];
+                }
+                else {
+                    output -= x[j][0];
+                }
+                // Console.WriteLine(z[^1]);
+                // Console.WriteLine(output);
+                j++;
+            }
+            // Console.ReadLine();
+        }
+        Console.WriteLine(output);
+    }
+}
